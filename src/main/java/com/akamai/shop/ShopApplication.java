72313c4dc4447fac39745815da1b4c895e31e9ca
Yosep Kim <yosepk@gmail.com>
2024-11-18 11:28:08 -0500

    enables CORS

--- src/main/java/com/akamai/shop/ShopApplication.java
+++ src/main/java/com/akamai/shop/ShopApplication.java
@@ -5,18 +5,30 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.BufferedImageHttpMessageConverter;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.awt.image.BufferedImage;
 
 @SpringBootApplication
 public class ShopApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ShopApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ShopApplication.class, args);
+    }
 
-	@Bean
-	public HttpMessageConverter<BufferedImage> bufferedImageHttpMessageConverter() {
-		return new BufferedImageHttpMessageConverter();
-	}
+    @Bean
+    public HttpMessageConverter<BufferedImage> bufferedImageHttpMessageConverter() {
+        return new BufferedImageHttpMessageConverter();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
 }
